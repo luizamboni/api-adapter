738f915e9d3b1a2ab59236817ec3de581493f158
Luiz Zamboni <luiz.vasconcellos@b2wdigital.com>
2018-05-23 12:27:45 -0300

    ADD self type

--- parser-properties.js
+++ parser-properties.js
@@ -1,38 +1,39 @@
-
 const ObjectPath = require("object-path")
 
-
 parseProperties = (props, data, opts = {}) => {
+ const newResponse = {}
+ 
+ for(const prop of props ) {
+   const { to, value, from , properties , middleware = [], required, self } = prop
 
-  const newResponse = {}
-  for(const prop of props ) {
-    const { to, value, from , properties , middleware = [], required } = prop
+   let value2
 
-    let value2
+   if (properties) {
+     const Root = ObjectPath.get(data, from)
 
-    if (properties) {
-      const Root = ObjectPath.get(data, from)
+     value2 = Root.map(el => (
+       parseProperties(properties, el)
+     ))
 
-      value2 = Root.map(el => (
-        parseProperties(properties, el)
-      ))
+     
+   } else {
+     value2 = from ? ObjectPath.get(data, from) : value
 
-       
-    } else {
-      value2 = from ? ObjectPath.get(data, from) : value 
+     if(!value2 && required)
+       throw new Error(`${to} is required`)
 
-      if(!value2 && required)
-        throw new Error(`${to} is required`)
+     middleware.forEach(funcName => {
+       value2 = (opts.middleware[funcName] || global[funcName])(value2)
+     })
+   }
 
-      middleware.forEach(funcName => {
-        value2 = (opts.middleware[funcName] || global[funcName])(value2)
-      })
-    }
+   if(self)
+     ObjectPath.set(data, self, value2)
+   else
+     ObjectPath.set(newResponse, to, value2)
+ }
 
-    ObjectPath.set(newResponse, to, value2)
-  }
-
-  return newResponse
+ return newResponse
 }
 
 module.exports = parseProperties
